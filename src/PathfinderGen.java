0a630845121eceb91692af602581db5e66e0aa12
Jonathan Dao <jonathandao0@gmail.com>
2018-03-16 13:52:34 -0700

    -Changed path array to an arraylist for ease of use

--- src/PathfinderGen.java
+++ src/PathfinderGen.java
@@ -1,30 +1,28 @@
 import java.io.File;
+import java.util.ArrayList;
 
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.Waypoint;
-
 
 public class PathfinderGen {
-	static Path[] path;
-	static int index;
+	static ArrayList<Path> path = new ArrayList<>();
 	
 	public static void main(String[] args){
 		initializePaths();
 
 		Trajectory trajectory;
-		for(int i = 0; i < path.length; i++) {
+		for(int i = 0; i < path.size(); i++) {
 			try {
 				System.out.println("Generating Path...");
-				trajectory = Pathfinder.generate(path[i].path, path[i].config);
+				trajectory = Pathfinder.generate(path.get(i).path, path.get(i).config);
 				System.out.println("Path Generated!");
 				
-				File csvFile = new File("Pathfinder/" + path[i].name + ".csv");		// Remember to rename a successful file, otherwise it will be overwritten
+				File csvFile = new File("Pathfinder/" + path.get(i).name + ".csv");		// Remember to rename a successful file, otherwise it will be overwritten
 				Pathfinder.writeToCSV(csvFile, trajectory);
 				if(csvFile.exists())
-					System.out.println("Success! File \"" + path[i].name + ".csv\" was generated!");
+					System.out.println("Success! File \"" + path.get(i).name + ".csv\" was generated!");
 				else
-					System.out.println("Error: File \"" + path[i].name + ".csv\" was not created");
+					System.out.println("Error: File \"" + path.get(i).name + ".csv\" was not created");
 			} catch(Exception e){
 				System.out.println("Error: " + e.getMessage());
 			}
@@ -32,8 +30,8 @@ public class PathfinderGen {
 	}
 	
 	static void initializePaths(){
-		path[index++] = new Path("driveStraight", Waypoints.driveStraight);
-		path[index++] = new Path("centerAutoLeft", Waypoints.centerAutoLeft);
-		path[index++] = new Path("centerAutoRight", Waypoints.centerAutoRight);
+		path.add(new Path("driveStraight", Waypoints.driveStraight));
+		path.add(new Path("centerAutoLeft", Waypoints.centerAutoLeft));
+		path.add(new Path("centerAutoRight", Waypoints.centerAutoRight));
 	}
 }
