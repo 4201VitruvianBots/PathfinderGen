506bcfde4e0efd1740f1415f3bec179299716d8d
Jonathan Dao <jonathandao0@gmail.com>
2018-03-17 14:01:21 -0700

    -Readded tankModifier

--- src/PathfinderGen.java
+++ src/PathfinderGen.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.modifiers.TankModifier;
 
 public class PathfinderGen {
 	static ArrayList<Path> path = new ArrayList<>();
-	
+	static TankModifier modifier;
 	public static void main(String[] args){
 		initializePaths();
 
@@ -17,12 +18,19 @@ public class PathfinderGen {
 				trajectory = Pathfinder.generate(path.get(i).path, path.get(i).config);
 				System.out.println("Path Generated!");
 				
-				File csvFile = new File("Pathfinder/" + path.get(i).name + ".csv");		// Remember to rename a successful file, otherwise it will be overwritten
-				Pathfinder.writeToCSV(csvFile, trajectory);
-				if(csvFile.exists())
-					System.out.println("Success! File \"" + path.get(i).name + ".csv\" was generated!");
-				else
-					System.out.println("Error: File \"" + path.get(i).name + ".csv\" was not created");
+				modifier = new TankModifier(trajectory).modify(0.7639);
+				
+				File leftCsvFile = new File("Pathfinder/" + path.get(i).name + "_Left.csv");		// Remember to rename a successful file, otherwise it will be overwritten
+				Pathfinder.writeToCSV(leftCsvFile, modifier.getLeftTrajectory());
+				File rightCsvFile = new File("Pathfinder/" + path.get(i).name + "_Right.csv");		// Remember to rename a successful file, otherwise it will be overwritten
+				Pathfinder.writeToCSV(rightCsvFile, modifier.getRightTrajectory());
+				if(leftCsvFile.exists() && rightCsvFile.exists()) {
+					System.out.println("Success! File \"" + path.get(i).name + "_Left.csv\" was generated!");
+					System.out.println("Success! File \"" + path.get(i).name + "_Right.csv\" was generated!");
+				} else {
+					System.out.println("Error: File \"" + path.get(i).name + "_Left.csv\" was not created");
+					System.out.println("Error: File \"" + path.get(i).name + "_Right.csv\" was not created");
+				}
 			} catch(Exception e){
 				System.out.println("Error: " + e.getMessage());
 			}
